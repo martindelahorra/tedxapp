42ef4f241b982d2da14f2c95fc68cdc4e04fc596
martindelahorra <martindelahorra@gmail.com>
2024-07-12 18:35:37 -0400

    Second commit Sidebar in App with Drawer

--- tedx-app/app/(tabs)/_layout.tsx
+++ tedx-app/app/(tabs)/_layout.tsx
@@ -1,37 +1,29 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './index'; // Adjust the import according to your file structure
+import TabTwoScreen from './explore';
+import { Image } from 'react-native';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+const Drawer = createDrawerNavigator();
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function App() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer.Navigator
+         screenOptions={{
+          headerTintColor: 'white',
+          headerRight: () => (
+            <Image
+              source={require('@/assets/images/TEDxLogo.png')}
+              style={{ width: 200, height: 40, marginLeft: 10 }} // Adjust the size and margin as needed
+            />
+          ), // Change this to your desired color
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Explore" component={TabTwoScreen} />
+          {/* Add other screens here */}
+      </Drawer.Navigator>
+  </GestureHandlerRootView>
   );
 }
