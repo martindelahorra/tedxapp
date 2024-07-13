b20990776153eb27b6f1de27e3bbf611282a3de0
mauffin <damiancardenas22@gmail.com>
2024-07-12 22:34:04 -0400

    feat: add fonts and colors,text

--- tedx-app/tailwind.config.js
+++ tedx-app/tailwind.config.js
@@ -3,7 +3,25 @@ module.exports = {
   //- content: [],
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'bg-primary': '#FFFFFF',
+          'bg-secundary': '#FF0000',
+          'bg-disabled':'#ADADAD',
+          'bg-insta':"#262626",
+          'bg-linkedin':"#262626",
+          'bg-mail':"#EA4335",
+          'bg-spk':"#DD3333",
+          'black': '#030925',
+          'title-spk': '#FF2D55',
+          'subtitle-spk':"#4A4E62",
+        },
+        fontFamily: {
+          Outfit: ['Outfit-thin'],
+          Roboto: ['RobotoMono-Regular'],
+          BigShoulders: ['BigShoulderText-thin']
+        },
+      },
     },
     plugins: [],
   }
