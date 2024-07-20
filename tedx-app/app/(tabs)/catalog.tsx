b4725d9953363565796292130d064373e41364e6
mauffin <damiancardenas22@gmail.com>
2024-07-20 01:39:00 -0400

    feat: #5 added info in the catalog file and assets in images

--- tedx-app/app/(tabs)/catalog.tsx
+++ tedx-app/app/(tabs)/catalog.tsx
@@ -1,52 +1,56 @@
-import react from 'react';
-import {View ,Text} from 'react-native';
+import react from "react";
+import { View, Text, Image } from "react-native";
 
-export default function TabThreeScreen(){
-  return(
-  <View className='flex-1 ' >
-    <View className=' bg-bg-secundary'>
-      <Text className='text-white text-5xl pt-20 font-[Sans] uppercase text-center' >
-        Official Speakers
-      </Text>
-      
-      <Text className='text-white text-xl p-3.5 font-[Alata]  text-justify' >
-      We are pleased to announce that our official 
-      speakers are confirmed!
-      Join us on August 6th in Santiago,
-      Chile for a unique day at TEDxVitacura.
-      </Text>
+export default function TabThreeScreen() {
+  return (
+    <View className="flex-1 bg-white ">
+      <View className="bg-black">
+        <Text className="text-white text-5xl pt-20 font-[Sans] uppercase text-center">
+          Official Speakers
+        </Text>
 
-      
-
-      
+        <Text className="text-white text-xl p-3.5 font-[Outfit]  text-justify">
+          We are pleased to announce that our official speakers are confirmed!
+          Join us on August 6th in Santiago, Chile for a unique day at
+          TEDxVitacura.
+        </Text>
       </View>
 
-      <View className='pt-10 bg-white'>
-        <Text className='text-gray text-4xl font-[Alata] text-center'>
-          Schedule
+      <Image
+        source={require("@/assets/images/waves.png")}
+        style={{ width: "100%", height: 100 }}
+      />
+      <View className="h-12">
+        <Text className="text-center">Waiting info Speaker .....</Text>
+      </View>
+      <View className="pt-10 pb-10 bg-white">
+        <Text className="text-deep-blue text-4xl uppercase font-[Sans] font-semibold  text-center">
+          Schedules
         </Text>
-        <Text className='text-subtitle-spk text-lg pt-3.5 font-[Outfit] text-center'>
-          10:00 AM - Opening Remarks
+        <Text className="text-pinktdx uppercase text-4xl pt-3.5 font-[Sans]  font-extrabold text-center">
+          august 6 th
         </Text>
-        <Text className='text-subtitle-spk text-lg pt-1.5 font-[Outfit] text-center'>
-          10:30 AM - Keynote Speaker: John Doe
-        </Text>
-        <Text className='text-subtitle-spk text-lg pt-1.5 font-[Outfit] text-center'>
-          11:30 AM - Break
-        </Text>
-        <Text className='text-subtitle-spk text-lg pt-1.5 font-[Outfit] text-center'>
-          12:00 PM - Panel Discussion
-        </Text>
-        <Text className='text-subtitle-spk text-lg pt-1.5 font-[Outfit] text-center'>
-          1:00 PM - Lunch
-        </Text>
-
-
-
+        <Image
+          source={require("@/assets/images/line.png")}
+          className=" mx-auto w-52 h-2"
+        />
       </View>
 
-
-  </View>
-
-  )
+  
+      <View className="bg-black h-96  pt-5 pl-3.5">
+        <Image
+          source={require("@/assets/images/TEDxLogo.png")}
+          className="w-36 h-8 "
+        />
+        <Text className="text-white font-[Outfit] pt-4 ">
+          © 2024 Todos los derechos reservados, bajo licencia TED Conferences,
+          LLC. Este evento TEDx independiente, TEDxVitacura, está organizado
+          bajo licencia de TED. En línea con la misión de difundir ideas, TED ha
+          establecido el programa TEDx, que comprende eventos locales
+          organizados de manera autónoma. Se prohíbe estrictamente el uso de la
+          marca TEDxVitacura fuera de este contexto.
+        </Text>
+      </View>
+    </View>
+  );
 }
