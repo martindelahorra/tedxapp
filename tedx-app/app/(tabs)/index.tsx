a4764fc2dee369529e7acdb4f177415f9729a636
martindelahorra <martindelahorra@gmail.com>
2024-07-26 12:41:03 -0400

    Commit app in dark mode

--- tedx-app/app/(tabs)/index.tsx
+++ tedx-app/app/(tabs)/index.tsx
@@ -8,7 +8,7 @@ import { Button, Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app</Text>
       <StatusBar style="auto" />
     </View>
   );
